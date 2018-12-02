20390f91756a5ce172fb505bb408088a30577f95
Dominic Schiller <schiller.dominic@outlook.com>
2018-12-02 10:50:36 +0100

    Create Readme
    Create readme content how to setup and run the project

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # TeaCo
 Ruby on Rails project containing the TeaCo server platform.
+
+<br />
+<br />
+**Course Instructor**
+
+* [Prof. Dr. rer. nat. Martin Christof Kindsmüller](mailto:martin.christof.kindsmueller@th-brandenburg.de)
+
+**Initial Developer**
+
+* Mathias Meusel
+
+**Development Team (for all TeaCo-Ionic extensions)**
+
+* [Dominic Schiller](mailto:dominic.schiller@th-brandenburg.de)<br />
+Project Manager & Mobile Application Developer
+* [Vasileios Roumoglou](mailto:vasileios.roumoglou@th-brandenburg.de) <br />
+Mobile Application Developer
+* [Iven Köthke](mailto:koethke@th-brandenburg.de) <br />
+Mobile Application Developer
+
+----------
+
+## Prerequisites
+
+
+To use and run this Ionic project, it is required that the development environment is set up as follows:
+
+
+#### Tools
+
+
+* [Ruby on Rails Development Kit](http://railsinstaller.org/en) must be installed (latest stable version recommended) in order to install all required gems and run the project.
+* [MySQL Server](https://dev.mysql.com/downloads/) must the installed, since TeaCo uses a MySQL database. (The Community Server is recommened and fits all requirements)
+* The MySQL Connector must be installed and setup with the mysql2 gem as described [here](https://tuanmsp.wordpress.com/2018/05/11/ruby-on-rails-mysql2-with-ruby-2-4-on-windows/)
+* [MySQL Workbench](https://dev.mysql.com/downloads/workbench/) can be installed optionally. This is a UI-based client for managing the MySQL Server.
+
+## Initializing the Project
+
+Once the project has been cloned or downloaded to the local system it has to be initialized though.
+<br />
+To install all production and development dependencies using `Bundler` navigate to the project''s root directory and run:
+
+```console
+foo@bar:~$ bundle install 
+```
+
+ This command will install all declared gems from the RoR project's `Gemfile`.
+ 
+## Setup the Teaco database
+In MySQL the database called `teaco5` muts be manually created.
+To initialize this database with all required tables, move to the project's root directory and run:
+
+```console
+foo@bar:~$ rails db:migrate
+```
+
+This command will take all migration definitions from the  `/db/migrate` directory and reconstruct the database's model based on them.
+
+
+## Run the project
+To finally run the project, simply enter the following:
+
+```console
+foo@bar:~$ rails server
+```
+
+This command will run the RoR server. The application itself will be accessible through the web browser by `http://localhost:3000`.
--- db/schema.rb
+++ db/schema.rb
@@ -12,78 +12,78 @@
 
 ActiveRecord::Schema.define(version: 20130810193901) do
 
-  create_table "alias_addresses", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "alias_addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "address", null: false
     t.integer "owner_id", null: false
     t.string "confirmation_hash", null: false
     t.boolean "confirmed", default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "comments", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "author_id", null: false
     t.integer "meeting_id", null: false
     t.text "text", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "known_addresses", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "known_addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "address"
     t.integer "owner_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "meetings", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "meetings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "initiator_id", null: false
     t.boolean "restricted", default: false, null: false
   end
 
-  create_table "meetings_users", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "meetings_users", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "meeting_id", null: false
     t.integer "user_id", null: false
   end
 
-  create_table "suggestions", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "suggestions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "meeting_id", null: false
     t.date "date", null: false
     t.time "start", null: false
     t.time "end", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "creator_id", null: false
     t.boolean "picked", default: false, null: false
   end
 
-  create_table "tokens", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "tokens", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "token", null: false
     t.integer "owner_id", null: false
     t.datetime "updated_at"
   end
 
-  create_table "users", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", default: "", null: false
     t.string "email", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "key", null: false
     t.string "language", default: "de", null: false
     t.string "freebusy_url", default: "", null: false
-    t.text "freebusy_data", limit: 16777215, null: false
+    t.text "freebusy_data", null: false
     t.boolean "watched_vote_tutorial", default: false, null: false
     t.boolean "watched_new_meeting_tutorial", default: false, null: false
     t.string "password_hash"
   end
 
-  create_table "votes", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "votes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "decision", default: "?", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "suggestion_id", null: false
     t.integer "voter_id", null: false
   end
