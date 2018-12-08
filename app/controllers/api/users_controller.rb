a8e17456268f45b8209bfa86b454a5ba2c5fef7e
Dominic Schiller <schiller.dominic@outlook.com>
2018-12-08 12:06:58 +0100

    Issue #4: Refactor function for loading a specific user
    Specific users can now be loaded independently from the routing lifecycle i.e. by another API application controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -20,13 +20,22 @@ module Api
     # (1) ID or
     # (2) hash key
     def show
+      user = UsersController.get_user(params)
+      self.send_json(user)
+    end
+
+    ##
+    # Fetches one specific user defined by it's unique
+    # (1) ID or
+    # (2) hash key
+    def self.get_user(params)
       key = params[:user_key]
       if key.match(/^(\d)+$/)
         # the key is an ID number, so fetch by ID
-        self.send_json(User.find(key))
+        return User.find(key)
       else
         # the key is the actual user hash key, so fetch by this property
-        self.send_json(User.find_by_key(key))
+        return User.find_by_key(key)
       end
     end
 
