9cf45cdb04a4f865cbb9e0879e88237f25bdf757
Dominic Schiller <schiller.dominic@outlook.com>
2019-02-10 09:08:42 +0100

    Issue #3: Redesign user confirmation email, added a fallback link for in-app registration, and implement app link generator

---
+++ app/helpers/app-link-generator.rb
@@ -0,0 +1,12 @@
+##
+# Utility class which generates custom mobile app deep links.
+class AppLinkGenerator
+  ##
+  # Generate a deep link to register a user within the TeaCo mobile app.
+  def self.generate_registration_link(user_key)
+    app_schema_prefix = "teaco://user/"
+    app_link << app_schema_prefix
+    app_link << user_key
+    app_link
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Teaco
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # set the default locale to German
+    config.i18n.default_locale = :de
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
