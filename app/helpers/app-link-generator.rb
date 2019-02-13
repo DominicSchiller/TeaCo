796f577c3da3693a6683763a9a0bd2abdf54e7e6
Dominic Schiller <schiller.dominic@outlook.com>
2019-02-13 14:34:35 +0100

    Issue #3: Update user confirmation email template

--- app/helpers/app-link-generator.rb
+++ app/helpers/app-link-generator.rb
@@ -5,6 +5,7 @@ class AppLinkGenerator
   # Generate a deep link to register a user within the TeaCo mobile app.
   def self.generate_registration_link(user_key)
     app_schema_prefix = "teaco://user/"
+    app_link = ""
     app_link << app_schema_prefix
     app_link << user_key
     app_link
