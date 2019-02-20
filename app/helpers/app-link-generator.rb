c1d2b0038e8b98377bcf3b284dc5ffc2a40cb031
Dominic Schiller <schiller.dominic@outlook.com>
2019-02-20 15:20:58 +0100

    Issue #2: Fix problem generating the correct meeting app link

--- app/helpers/app-link-generator.rb
+++ app/helpers/app-link-generator.rb
@@ -4,22 +4,14 @@ class AppLinkGenerator
   ##
   # Generate a deep link to register a user within the TeaCo mobile app.
   def self.generate_registration_link(user_key)
-    app_schema_prefix = "teaco://user/"
-    app_link = ""
-    app_link << app_schema_prefix
-    app_link << user_key
+    app_link = "teaco://user/#{user_key}"
     app_link
   end
 
   ##
   # Generate a deep link to show specific meeting within the TeaCo mobile app.
   def self.generate_meeting_link(user_key, meeting_id)
-    app_schema_prefix = "teaco://user/"
-    app_link = ""
-    app_link << app_schema_prefix
-    app_link << user_key
-    app_link << "meetings/"
-    app_link << meeting_id
+    app_link = "teaco://user/#{user_key}/meetings/#{meeting_id}"
     app_link
   end
 end
